883864895e5e841ca296af97cb19b1d97b030f18
GaLLium-git <205247616+GaLLium-git@users.noreply.github.com>
2025-10-12 11:54:18 +0900

    Update rollinghash.rs

--- string/rollinghash.rs
+++ string/rollinghash.rs
@@ -1,26 +1,29 @@
 fn main(){
+    let s:Vec<char>="atcatc".chars().collect();
+    let mut rol=RollingHash::new(&s,10);
+    if rol.hash(0,3)==rol.hash(3,6){
+        println!("ok");
+    }
 }
 
+type ModInt1=ac_library::ModInt1000000007;
+type ModInt2=ac_library::ModInt998244353;
 pub struct RollingHash {
-    hash1: Vec<StaticModInt>,
-    hash2: Vec<StaticModInt>,
+    base:usize,
+    hash1: Vec<ModInt1>,
+    hash2: Vec<ModInt2>,
 }
-
 impl RollingHash {
-    const MOD1=1000000007;
-    const MOD2=998244353;
-    pub fn new(s: &Vec<char>, base: u64) -> Self {
+    pub fn new(s: &Vec<char>, base: usize) -> Self {
         let n = s.len();
-        let mut hash1 = vec![0; n + 1];
-        
-        for i in 0..n{
-            hash1[i + 1] = (hash1[i] * base + s[i] as usize
+        let mut hash1 = vec![ModInt1::new(0);n+1];
+        for i in 1..=n{
+            hash1[i]=hash1[i-1]*ModInt1::new(base)+ModInt1::new(s[i-1] as u32);
         }
-        for (i, c) in s.bytes().enumerate() {
-            hash[i + 1] = (hash[i] * base + c as u64) % modulo;
-            power[i + 1] = (power[i] * base) % modulo;
+        let mut hash2 = vec![ModInt2::new(0);n+1];
+        for i in 1..=n{
+            hash2[i]=hash2[i-1]*ModInt2::new(base)+ModInt2::new(s[i-1] as u32);
         }
-
         RollingHash {
             base,
             hash1,
@@ -28,10 +31,10 @@ impl RollingHash {
         }
     }
 
-    pub fn hash(&self, l: usize, r: usize) -> (StaticModInt<MOD1>,StaticModInt<MOD2>) {
-        let res1=hash1[r-1]-hash1[l-1]*StaticModInt<MOD1>::new(BASE).pow(r-l);
-        let res2=hash2[r-1]-hash2[l-1]*StaticModInt<MOD2>::new(BASE).pow(r-l);
+    //sの[l,r)のハッシュ(0indexed)
+    pub fn hash(&self, l: usize, r: usize) -> (ModInt1,ModInt2) {
+        let res1=self.hash1[r]-self.hash1[l]*ModInt1::new(self.base).pow((r-l) as u64);
+        let res2=self.hash2[r]-self.hash2[l]*ModInt2::new(self.base).pow((r-l) as u64);
         (res1,res2)
     }
-
 }
