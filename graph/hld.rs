93abe9ee539f73a727fc1121f76498d39430f854
GaLLium-git <205247616+GaLLium-git@users.noreply.github.com>
2025-10-27 19:01:52 +0900

    Create HLD structure with placeholder methods
    Initial implementation of HLD structure with placeholder methods.

---
+++ graph/hld.rs
@@ -0,0 +1,20 @@
+fn main(){
+    //test
+}
+
+
+
+use ac_library::Monoid;
+pub struct HLD<M:Monoid>{
+   
+}
+
+impl<M:Monoid> HLD<M>{
+   fn new()
+   
+   fn lca()
+   
+   fn set()
+   
+   fn query()
+}
