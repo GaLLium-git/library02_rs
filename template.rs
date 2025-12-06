b7b9167a7f81abecd15a44a90a18ae3872c15794
GaLLium-git <205247616+GaLLium-git@users.noreply.github.com>
2025-12-06 13:44:55 +0900

    Update template.rs

--- template.rs
+++ template.rs
@@ -29,12 +29,12 @@ impl Scanner {
         Scanner {buffer: std::collections::VecDeque::new()}
     }
     pub fn next<T: std::str::FromStr>(&mut self) -> T {
-        if buffer.len() == 0 {
+        if self.buffer.len() == 0 {
             let mut input = String::new();
             std::io::stdin().read_line(&mut input).unwrap();
             self.buffer = input.split_whitespace().map(|s| s.to_string()).collect();
         }
-        self.buffer.pop_front().parse::<T>().unwrap();
+        self.buffer.pop_front().unwrap().parse::<T>().ok().unwrap()
     }
 }
 
