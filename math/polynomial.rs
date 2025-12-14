b2973f56844a74b28749cd0f5295ab2420b77c4f
GaLLium-git <205247616+GaLLium-git@users.noreply.github.com>
2025-12-14 09:24:43 +0900

    Update polynomial.rs

--- math/polynomial.rs
+++ math/polynomial.rs
@@ -1,10 +1,14 @@
 fn main(){
-    let A = Poly::new(vec![Mint::new(1),Mint::new(2),Mint::new(3)]); //1 + 2x + 2x^2
+    let A = Poly::new(vec![Mint::new(1),Mint::new(2),Mint::new(3)]); //1 + 2x + 3x^2
     let B = Poly::new(vec![Mint::new(4),Mint::new(5)]); //4 + 5x
     
-    eprintln!("{:?}",(A.clone()+B.clone()).seq);
+    eprintln!("{:?}",(A.clone()+B.clone()).seq); 
+    
+    eprintln!("{:?}",(A.clone()-B.clone()).seq);
+    
     eprintln!("{:?}",(A.clone()*B.clone()).seq);
-    eprintln!("{:?}",(A.clone()*Mint::new(-1)).seq);
+    
+    eprintln!("{}",A.clone().assign(Mint::new(2)));
 }
 
 
@@ -21,11 +25,50 @@ impl Poly{
     fn new (seq:Vec<Mint>) -> Self{
         Self{seq}
     }
+    
+    fn zero() -> Self{
+        Self{seq:vec![Mint::new(0)]}
+    }
+    
+    fn inv(self) -> Self{
+        Self::zero()  
+    }
+
+    fn bibun(self) -> Self{
+        Self::zero()  
+    }
+    
+    fn sekibun(self) -> Self{
+        Self::zero()  
+    }
+    
+    fn log(self) -> Self{
+        Self::zero()
+    }
+    
+    fn exp(self) -> Self{
+        Self::zero()
+    }
+    
+    //値の代入 O(N)
+    fn assign(self,c:Mint) -> Mint{
+        let mut res = Mint::new(0);
+        let mut pow = Mint::new(1);
+        for i in 0..self.seq.len(){
+            res += self.seq[i]*pow;
+            pow *= c;
+        }
+        res
+    }
+    
+    fn taylor_shift(self,c:Mint) -> Self{
+        Self::zero()
+    }
 }
 
 
 use std::ops::{Add,Sub,Mul,Div};
-//和 O(N)
+//加法 O(N)
 impl Add for Poly{
     type Output = Self;
     fn add(self, rhs:Self) -> Self {
@@ -43,6 +86,14 @@ impl Add for Poly{
     }
 }
 
+//減法 O(N)
+impl Sub for Poly{
+    type Output = Self;
+    fn sub(self, rhs:Self) -> Self {
+        self + rhs * Mint::new(-1)
+    }
+}
+
 //定数倍 O(N) f*cの形のみ可
 impl Mul<Mint> for Poly{
     type Output = Self;
@@ -55,12 +106,20 @@ impl Mul<Mint> for Poly{
     }
 }
 
-//積 O(NlogN)
+//乗法 O(NlogN)
 impl Mul for Poly{
     type Output = Self;
     fn mul(self, rhs:Self) -> Self {
         let mut res = ac_library::convolution(&self.seq,&rhs.seq);
         res.truncate(MAX_DEG);
         Self{seq: res}
+    }
+}
+
+//除法 O(NlogN);
+impl Div for Poly{
+    type Output = Self;
+    fn div(self, rhs:Self) -> Self {
+        self * rhs.inv()
     }
 }
