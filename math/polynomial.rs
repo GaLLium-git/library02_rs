af20f60567f440a31872df78a4d76dfdb874e8e8
GaLLium-git <205247616+GaLLium-git@users.noreply.github.com>
2025-12-14 13:03:53 +0900

    Update polynomial.rs

--- math/polynomial.rs
+++ math/polynomial.rs
@@ -2,13 +2,15 @@ fn main(){
     let A = Poly::new(vec![Mint::new(1),Mint::new(2),Mint::new(3)]); //1 + 2x + 3x^2
     let B = Poly::new(vec![Mint::new(4),Mint::new(5)]); //4 + 5x
     
-    eprintln!("{:?}",(A.clone()+B.clone()).seq); 
-    
-    eprintln!("{:?}",(A.clone()-B.clone()).seq);
-    
-    eprintln!("{:?}",(A.clone()*B.clone()).seq);
-    
-    eprintln!("{}",A.clone().assign(Mint::new(2)));
+    eprintln!("A={:?},B={:?}",A,B);
+    eprintln!("A+B={:?}",A.clone()+B.clone()); 
+    eprintln!("A-B={:?}",A.clone()-B.clone());
+    eprintln!("A*B={:?}",A.clone()*B.clone());
+    eprintln!("A/B={:?}",A.clone()/B.clone());
+    eprintln!("A'={:?}",A.clone().bibun());
+    eprintln!("∫A={:?}",A.clone().sekibun());
+    eprintln!("log A={:?}",A.clone().log());
+    eprintln!("A(2)={}",A.clone().assign(Mint::new(2)));
 }
 
 
@@ -16,7 +18,7 @@ fn main(){
 type Mint = ac_library::ModInt998244353;
 const MAX_DEG:usize = 1000000; //次数は < 1e6 で打ち切る
 
-#[derive(Clone,PartialEq,Eq)]
+#[derive(Clone,Debug,PartialEq,Eq)]
 pub struct Poly{
     pub seq:Vec<Mint>
 }
@@ -30,24 +32,48 @@ impl Poly{
         Self{seq:vec![Mint::new(0)]}
     }
     
+    //多項式逆元 O(NlogN)
     fn inv(self) -> Self{
-        Self::zero()  
+        let mut res = Poly::new(vec![Mint::new(1)/self.seq[0]]);
+        for _ in 0..=MAX_DEG.ilog2(){
+            res = res.clone()*Mint::new(2) - self.clone()*res.clone()*res.clone();
+        }
+        res.seq.truncate(MAX_DEG);
+        res 
     }
-
+    
+    //微分 O(N)
     fn bibun(self) -> Self{
-        Self::zero()  
+        let mut res = vec![];
+        for i in 1..self.seq.len(){
+            res.push(self.seq[i]*Mint::new(i));
+        }
+        Self{seq: res}
     }
     
+    //積分 O(N)
     fn sekibun(self) -> Self{
-        Self::zero()  
+        let mut res = vec![Mint::new(0)];
+        for i in 0..self.seq.len(){
+            res.push(self.seq[i] / Mint::new(i+1));
+        }
+        res.truncate(MAX_DEG);
+        Self{seq: res}
     }
     
+    //多項式のlog O(NlogN)
     fn log(self) -> Self{
-        Self::zero()
+        (self.clone().bibun() / self.clone()).sekibun()
     }
     
+    //多項式のexp O(NlogN)
     fn exp(self) -> Self{
         Self::zero()
+    }
+    
+    //累乗 O(NlogN)
+    fn pow(self, n:usize) -> Self{
+        (self.log() * Mint::new(n)).exp() 
     }
     
     //値の代入 O(N)
