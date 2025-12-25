d743f9e4191e2a5f1515034e90f5ad4d8e9aa021
GaLLium-git <205247616+GaLLium-git@users.noreply.github.com>
2025-12-25 14:05:01 +0900

    Create dequecht.rs

---
+++ data structure/dequecht.rs
@@ -0,0 +1,14 @@
+//CHT
+//追加クエリ:単調，交点を求める計算量に依存
+//取得クエリ:単調ならO(1) else O(logN)
+struct DequeCHT<T,F>{
+    
+}
+
+impl DequeCHT{
+    pub fn new()
+    fn intersect
+    pub fn add(&mut self,F)
+    pub fn get(self,) -> T
+    pub fn get_monotone(self,)
+}
