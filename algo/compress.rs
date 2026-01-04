47a7d2226c2b2fd81abe1c5a87422d360952f295
GaLLium-git <205247616+GaLLium-git@users.noreply.github.com>
2026-01-04 12:23:38 +0900

    Update compress.rs

--- algo/compress.rs
+++ algo/compress.rs
@@ -1,19 +1,13 @@
-//座標圧縮
-fn main() {
-   let mut a=vec![0,0,1,1,2,2,5,5,4,4,3,3];
-   let mut b=compress(&a);
-   println!("{}",b.iter().join("\n"));
-}
-pub fn compress<T:Ord + Copy> (a:&Vec<T>)->Vec<usize>{
-    let mut t=Vec::new();
+//座圧
+pub fn compress<T:Ord + Copy> (a:&Vec<T>) -> Vec<usize>{
+    let mut t = Vec::new();
     for i in 0..a.len(){
         t.push(a[i]);
     }
-    t.sort();
-    t.dedup();
-    let mut b=Vec::new();
+    t.sort(); t.dedup();
+    let mut b = Vec::new();
     for i in 0..a.len(){
-        b.push(bsearch_usize(0,t.len(),|x| t[x]>=a[i]))
+        b.push(bsearch_usize(0..t.len(),|x| t[x]>=a[i]))
     }
     b
 }
