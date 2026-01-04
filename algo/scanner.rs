03cfd94a8d8c30afde4772965814738a6e270713
GaLLium-git <205247616+GaLLium-git@users.noreply.github.com>
2026-01-04 12:22:37 +0900

    Update scanner.rs

--- algo/scanner.rs
+++ algo/scanner.rs
@@ -1,38 +1,17 @@
-//標準入力を受け取るやつ VecDeque<String>を持って1行ずつ受け取る(インタラクティブにも使える．)
-fn main(){
-    let mut sc=Scanner::new();
-    let n:usize=sc.next();
-    println!("{}",n);
-    let a:Vec<usize>=(0..n).map(|_| sc.next()).collect();
-    println!("{:?}",a);
-}
-
+//Scanner
 pub struct Scanner {
     buffer: std::collections::VecDeque<String>,
 }
 impl Scanner {
     pub fn new() -> Self {
-        Scanner {
-            buffer: std::collections::VecDeque::new(),
-        }
+        Scanner {buffer: std::collections::VecDeque::new()}
     }
-
-    fn read_line(&mut self) {
-        let mut input = String::new();
-        std::io::stdin().read_line(&mut input).unwrap();
-        self.buffer = input
-            .split_whitespace()
-            .map(|s| s.to_string())
-            .collect();
-    }
-
     pub fn next<T: std::str::FromStr>(&mut self) -> T {
-        loop {
-            if let Some(token) = self.buffer.pop_front() {
-                return token.parse::<T>().ok().unwrap();
-            }
-            self.read_line();
+        if self.buffer.len() == 0 {
+            let mut input = String::new();
+            std::io::stdin().read_line(&mut input).unwrap();
+            self.buffer = input.split_whitespace().map(|s| s.to_string()).collect();
         }
+        self.buffer.pop_front().unwrap().parse::<T>().ok().unwrap()
     }
 }
-
