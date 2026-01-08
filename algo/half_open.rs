2e5a32cc5e093af44e34c6ef6871e75f12ec2b9e
GaLLium-git <205247616+GaLLium-git@users.noreply.github.com>
2026-01-08 15:31:15 +0900

    Create half_open.rs

---
+++ algo/half_open.rs
@@ -0,0 +1,14 @@
+//usizeのrange型を[l,r)に直す関数]
+pub fn half_open_usize(range: impl std::ops::RangeBounds<usize>) -> (usize,usize){
+    let mut l = match range.start_bound() {
+        std::ops::Bound::Included(l) => *l,
+        std::ops::Bound::Excluded(l) => l+1,
+        std::ops::Bound::Unbounded => 0,
+    };
+    let mut r = match range.end_bound() {
+        std::ops::Bound::Included(r) => r+1,
+        std::ops::Bound::Excluded(r) => *r,
+        std::ops::Bound::Unbounded => usize::MAX,
+    };
+    (l,r)
+}
