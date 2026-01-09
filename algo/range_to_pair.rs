ff480b8c73431dd77417c661317850bf0b820476
GaLLium-git <205247616+GaLLium-git@users.noreply.github.com>
2026-01-09 12:20:21 +0900

    Refactor range_to_pair function for clarity

--- algo/range_to_pair.rs
+++ algo/range_to_pair.rs
@@ -1,12 +1,12 @@
 //usizeのrange型を[l,r)に直す関数]
 pub fn range_to_pair(range: impl std::ops::RangeBounds<usize>) -> (usize,usize){
-    let mut l = match range.start_bound() {
+    let l = match range.start_bound() {
         std::ops::Bound::Included(l) => *l,
-        std::ops::Bound::Excluded(l) => l+1,
+        std::ops::Bound::Excluded(l) => *l+1,
         std::ops::Bound::Unbounded => 0,
     };
-    let mut r = match range.end_bound() {
-        std::ops::Bound::Included(r) => r+1,
+    let r = match range.end_bound() {
+        std::ops::Bound::Included(r) => *r+1,
         std::ops::Bound::Excluded(r) => *r,
         std::ops::Bound::Unbounded => usize::MAX,
     };
