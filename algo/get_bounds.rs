4e78ffa207b9b82544f743b3d5cafd0a7fe826d5
GaLLium-git <205247616+GaLLium-git@users.noreply.github.com>
2026-01-09 12:39:47 +0900

    Rename range_to_pair to get_bounds_usize

--- algo/get_bounds.rs
+++ algo/get_bounds.rs
@@ -1,5 +1,5 @@
 //usizeのrange型を[l,r)に直す関数]
-pub fn range_to_pair(range: impl std::ops::RangeBounds<usize>) -> (usize,usize){
+pub fn get_bounds_usize(range: impl std::ops::RangeBounds<usize>) -> (usize,usize){
     let l = match range.start_bound() {
         std::ops::Bound::Included(l) => *l,
         std::ops::Bound::Excluded(l) => *l+1,
