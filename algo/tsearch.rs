9a5f99ee319495af33514e3fe6ae11abde32ad96
GaLLium-git <205247616+GaLLium-git@users.noreply.github.com>
2026-01-10 20:38:16 +0900

    Update tsearch.rs

--- algo/tsearch.rs
+++ algo/tsearch.rs
@@ -1,20 +1,20 @@
 //三分探索
-//中身は関数名の通り、型は問題に応じて変えよう
 fn main() {
-    let f = |x: usize| -(x - 5) * (x - 5);
-    let x = tsearch_imax(0, 11, f); // [0, 11) → x ∈ [0,10]
+    let f = |x: usize| (x - 5) * (x - 5);
+    let x = tsearch_max_usize(0..11, f); 
     println!("imax: x = {}, f(x) = {}", x, f(x));
 
     let g = |x: f64| (x - 2.0).powi(2) + 1.0;
-    let x = tsearch_fmin(0.0, 5.0, g, 1e-9);
+    let x = tsearch_min_f64(0.0..5.0, g, 1e-9);
     println!("fmin: x = {:.9}, f(x) = {:.9}", x, g(x));
 }
 
 
-pub fn tsearch_imax<F>(mut l: usize, mut r:usize, f: F) -> usize
+pub fn tsearch_max_usize<F>(range: impl std::ops::RangeBounds<usize>, f: F) -> usize
 where
     F: Fn(usize) -> usize,
 {
+    let (mut l, mut r) = get_bounds_usize(range);
     while r - l > 3 {
         let m1 = l + (r - l) / 3;
         let m2 = r - (r - l) / 3;
@@ -27,10 +27,11 @@ where
     (l..r).max_by_key(|&x| f(x)).unwrap()
 }
 
-pub fn tsearch_imin<F>(mut l: usize, mut r: usize, f: F) -> usize
+pub fn tsearch_min_usize<F>(range: impl std::ops::RangeBounds<usize>, f: F) -> usize
 where
     F: Fn(usize) -> usize,
 {
+    let (mut l, mut r) = get_bounds_usize(range);
     while r - l > 3 {
         let m1 = l + (r - l) / 3;
         let m2 = r - (r - l) / 3;
@@ -43,10 +44,11 @@ where
     (l..r).min_by_key(|&x| f(x)).unwrap()
 }
 
-pub fn tsearch_fmax<F>(mut l: f64, mut r: f64, f: F, eps: f64) -> f64
+pub fn tsearch_max_f64<F>(range: impl std::ops::RangeBounds<f64>, f: F, eps: f64) -> f64
 where
     F: Fn(f64) -> f64,
 {
+    let (mut l, mut r) = get_bounds_f64(range);
     while r - l > eps {
         let m1 = l + (r - l) / 3.0;
         let m2 = r - (r - l) / 3.0;
@@ -59,10 +61,11 @@ where
     (l + r) / 2.0
 }
 
-pub fn tsearch_fmin<F>(mut l: f64, mut r: f64, f: F, eps: f64) -> f64
+pub fn tsearch_min_f64<F>(range: impl std::ops::RangeBounds<f64>, f: F, eps: f64) -> f64
 where
     F: Fn(f64) -> f64,
 {
+    let (mut l, mut r) = get_bounds_f64(range);
     while r - l > eps {
         let m1 = l + (r - l) / 3.0;
         let m2 = r - (r - l) / 3.0;
