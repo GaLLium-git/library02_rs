59f022dbdd0355a2530cd6d95a28258b706e44b9
GaLLium-git <205247616+GaLLium-git@users.noreply.github.com>
2026-01-13 09:34:37 +0900

    Update and rename mintprecalc.rs to binom.rs

---
+++ math/binom.rs
@@ -0,0 +1,29 @@
+type Mint = ac_library::ModInt998244353;
+pub struct Binom{
+    fac: Vec<Mint>,
+    ifac: Vec<Mint>,
+}
+
+impl Binom{
+    pub fn new(MAX:usize) -> Self{
+        let mut fac = vec![Mint::new(1);MAX+1];
+        for i in 1..=MAX{
+            fac[i] = fac[i-1] * Mint::new(i);
+        }
+        let mut ifac = vec![Mint::new(1);MAX+1];
+        ifac[MAX] = fac[MAX].inv();
+        for i in (1..=MAX).rev(){
+            ifac[i-1] = ifac[i] * Mint::new(i);
+        }
+        Self{fac,ifac}
+    }
+
+    pub fn C(n:usize, k:usize) -> Mint{
+        if n < k {return 0;}
+        self.fac[n]*self.ifac[n-k]*self.fac[k]
+    }
+
+     pub fn H(n:usize, k:usize) -> Mint{
+        self.C(n+k-1,k)
+    }
+}
