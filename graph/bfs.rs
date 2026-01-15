2589338f6eb121098eb8b541dcb20964fcbbf3d4
GaLLium-git <205247616+GaLLium-git@users.noreply.github.com>
2026-01-15 13:27:19 +0900

    Update bfs.rs

--- graph/bfs.rs
+++ graph/bfs.rs
@@ -14,20 +14,4 @@ pub fn bfs(graph: &Vec<Vec<usize>>, root:usize) -> Vec<usize>{
     }
     dist
 }
-
-fn main() {
-    let mut sc = Scanner::new();
-    let (n,m):(usize,usize) = (sc.next(),sc.next());
-    let mut graph=vec![vec![];n+1];
-    for i in 1..=m{
-      let (a,b):(usize,usize)=(sc.next(),sc.next());
-      graph[a].push(b);
-      graph[b].push(a);
-    }
-    let dist = bfs(&graph,1);
-    for i in 1..=n{
-        println!("{}",dist[i]);
-    }
-    
-}
     
