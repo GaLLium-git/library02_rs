8d4fce9bb4e6eb2a09e5ace22d068d8f6927189c
GaLLium-git <205247616+GaLLium-git@users.noreply.github.com>
2026-01-17 19:16:10 +0900

    Update waveletmatrix.rs

--- data structure/waveletmatrix.rs
+++ data structure/waveletmatrix.rs
@@ -1,27 +1,35 @@
 //Wavelet Matrix
 //座圧したほうがいい？
 pub struct WaveletMatrix{
-    data: Vec<bitvec>,
+    data: Vec<BitVec>,
 }
 
 impl WaveletMatrix{
     pub fn new(val:&Vec<usize>) -> Self{
         let log = val.iter().max().unwrap().ilog2();
-        let mut data = vec![bitvec::new(val.len());log+1];
-        for i in (0..=log).rev(){
-            for j in 0..val.len(){
-                if val[j] >> i == 1{
-                    data[i].set(j);
+        let mut data = vec![BitVec::new(val.len());log+1];
+        let mut cur = val.clone();
+        for b in (0..=log).rev(){
+            for i in 0..cur.len(){
+                if cur[i] >> b == 1{
+                    data[b].set(i);
                 }
             }
             data[i].build();
+            let mut nxt = cur.clone();
+            for i in 0..cur.len(){
+                if data[b].access(i) == 0{
+                    nxt[data[b].rank0(i)] = cur[i];
+                } else{
+                    nxt[data[b].rank1(i) + data[b].zero] = cur[i];
+                }
+            }
+            cur = nxt;
         }
-        Self{
-            data,
-        }
+        Self{data}
     }
     
-    
+    //T[i]を得る
     pub fn access(&self, i:usize) -> usize{
         let mut res = 0;
         let mut idx = i;
@@ -30,7 +38,7 @@ impl WaveletMatrix{
             if self.data[b].access(idx) == 0{
                 idx = self.data[b].rank0(idx)
             } else {
-                idx = self.data[b].rank1(idx) + self.data[b].count_zeros();
+                idx = self.data[b].rank1(idx) + self.data[b].zero;
             }
         }
         res
@@ -60,20 +68,31 @@ impl WaveletMatrix{
         self.kth_smallest(range,r-l-k-1)
     }
     
+    //rangeに存在するx未満の最大値
+    pub fn prev_value(&self, range: impl std::ops::RangeBounds<usize>, x:usize) -> usize{
+        let (mut l, mut r) = get_bounds_usize(range);
+    }
+    
+    //rangeに存在するx以上の最小値
+    pub fn next_value(&self, range: impl std::ops::RangeBounds<usize>, x:usize) -> usize{
+        let (mut l, mut r) = get_bounds_usize(range);
+    }
     
 }
 
 //ビット列に対する操作
-struct bitvec{
+struct BitVec{
     bits: Vec<usize>,
     sum: Vec<usize>,
+    zero: usize,
 }
 
-impl bitvec{
+impl BitVec{
     pub fn new(n:usize) -> Self{
         Self{
             bits: vec![0;n/64+1],
             sum: vec![0;n/64+1],
+            zero: n,
         }
     }
     
@@ -86,6 +105,7 @@ impl bitvec{
         for i in 1..=n/64{
             self.sum[i] = self.sum[i-1] + self.bits[i].counts_ones(); 
         }
+        self.zero -= self.sum.last.unwrap();
     }
     
     pub fn access(&self, i:usize) -> usize{
@@ -99,10 +119,5 @@ impl bitvec{
     
     pub fn rank0(&self, i:usize) -> usize{
         i - self.rank1(i)
-    }
-    
-    //全体での0の個数
-    pub fn count_zeros(&self) -> usize{
-        self.rank0(self.bits.len()*64)
     }
 }
