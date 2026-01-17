20a287efc2049db13ee16aceac9695ae6426a346
GaLLium-git <205247616+GaLLium-git@users.noreply.github.com>
2026-01-17 16:08:08 +0900

    Update rollinghash.rs

--- string/rollinghash.rs
+++ string/rollinghash.rs
@@ -6,23 +6,23 @@ fn main(){
     }
 }
 
-type ModInt1=ac_library::ModInt1000000007;
-type ModInt2=ac_library::ModInt998244353;
-pub struct RollingHash {
+type ModInt1 = ac_library::ModInt1000000007;
+type ModInt2 = ac_library::ModInt998244353;
+pub struct RollingHash{
     base:usize,
     hash1: Vec<ModInt1>,
     hash2: Vec<ModInt2>,
 }
 impl RollingHash {
-    pub fn new(s: &Vec<char>, base: usize) -> Self {
+    pub fn new(s: &Vec<char>, base: usize) -> Self{
         let n = s.len();
         let mut hash1 = vec![ModInt1::new(0);n+1];
         for i in 1..=n{
-            hash1[i]=hash1[i-1]*ModInt1::new(base)+ModInt1::new(s[i-1] as u32);
+            hash1[i] = hash1[i-1]*ModInt1::new(base)+ModInt1::new(s[i-1] as u32);
         }
         let mut hash2 = vec![ModInt2::new(0);n+1];
         for i in 1..=n{
-            hash2[i]=hash2[i-1]*ModInt2::new(base)+ModInt2::new(s[i-1] as u32);
+            hash2[i] = hash2[i-1]*ModInt2::new(base)+ModInt2::new(s[i-1] as u32);
         }
         RollingHash {
             base,
@@ -32,9 +32,9 @@ impl RollingHash {
     }
 
     //sの[l,r)のハッシュ(0indexed)
-    pub fn hash(&self, l: usize, r: usize) -> (ModInt1,ModInt2) {
-        let res1=self.hash1[r]-self.hash1[l]*ModInt1::new(self.base).pow((r-l) as u64);
-        let res2=self.hash2[r]-self.hash2[l]*ModInt2::new(self.base).pow((r-l) as u64);
+    pub fn hash(&self, l: usize, r: usize) -> (ModInt1,ModInt2){
+        let res1 = self.hash1[r]-self.hash1[l]*ModInt1::new(self.base).pow((r-l) as u64);
+        let res2 = self.hash2[r]-self.hash2[l]*ModInt2::new(self.base).pow((r-l) as u64);
         (res1,res2)
     }
 }
