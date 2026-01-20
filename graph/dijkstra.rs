9245ff6813d5a8df1aa51878c41037f7dd262b95
GaLLium-git <205247616+GaLLium-git@users.noreply.github.com>
2026-01-20 20:45:49 +0900

    Update dijkstra.rs

--- graph/dijkstra.rs
+++ graph/dijkstra.rs
@@ -1,31 +1,15 @@
 pub fn dijkstra(graph:&Vec<Vec<usize>>, root:usize) -> Vec<usize>{
-    
-}
-
-fn main() {
-    let mut sc = Scanner::new();
-    let (N,M):(usize,usize) = (sc.next(),sc.next());
-    let mut graph = vec![vec![];N+1];
-    for i in 1..=M{
-        let (a,b,c):(usize,usize,usize)=(sc.next(),sc.next(),sc.next());
-        graph[a].push((b,c));
-        graph[b].push((a,c));
-    }
-
     use std::cmp::Reverse;
-    let mut kakutei = vec![false;N+1];
+    let mut check = vec![false;N+1];
     let mut cur = vec![usize::MAX;N+1];
     let mut pq = BinaryHeap::new();
-    cur[1] = 0;
-    pq.push(Reverse((cur[1],1)));
+    cur[root] = 0;
+    pq.push(Reverse((cur[root],root)));
     
     while !(pq.is_empty()) {
         let Reverse((_,pos)) = pq.pop().unwrap();
-        if kakutei[pos] == true{
-            continue;
-        }
-        
-        kakutei[pos]=true;
+        if check[pos]{continue;}
+        check[pos] = true;
         for &(nex,cost) in graph[pos].iter(){
             if cur[nex] > cur[pos]+cost{
                 cur[nex] = cur[pos]+cost;
@@ -33,15 +17,6 @@ fn main() {
             }            
         }
     }
-    
-    for i in 1..=n{
-        if cur[i] == usize::MAX{
-            println!("{}",-1);
-        } else{
-            println!("{}",cur[i]);
-        }
-    }
-    
+    cur
 }
-    
-    
+
