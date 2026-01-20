200c19680a2967fd5bd15896dfacd0403821f23b
GaLLium-git <205247616+GaLLium-git@users.noreply.github.com>
2026-01-20 20:38:22 +0900

    Update bellmanford.rs

--- graph/bellmanford.rs
+++ graph/bellmanford.rs
@@ -1,12 +1,5 @@
-fn main() {
-    let mut sc=Scanner::new();
-    let (n,m):(usize,usize)=(sc.next(),sc.next());
-    let mut edges=vec![];
-    for i in 0..m{
-        let (a,b,c):(usize,usize,i64)=(sc.next(),sc.next(),sc.next());
-        edges.push((a,b,-c));
-    }
-    let mut d=vec![i64::MAX;n+1];
+pub fn bellmanford(edges: &Vec<(usize,usize,i64)>, root:usize) -> Vec<i64>{
+    let mut d = vec![i64::MAX;n+1];
     d[1]=0;
     for _ in 0..n-1{
         for &(u,v,cost) in edges.iter(){
@@ -22,6 +15,17 @@ fn main() {
             }
         }
     }
+
+}
+fn main() {
+    let mut sc=Scanner::new();
+    let (n,m):(usize,usize)=(sc.next(),sc.next());
+    let mut edges=vec![];
+    for i in 0..m{
+        let (a,b,c):(usize,usize,i64)=(sc.next(),sc.next(),sc.next());
+        edges.push((a,b,-c));
+    }
+    
     
     
     if d[n]==i64::MIN{
