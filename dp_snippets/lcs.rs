85357fbb16098985caa803ba77283647bcb620b2
GaLLium-git <205247616+GaLLium-git@users.noreply.github.com>
2026-01-24 18:58:15 +0900

    Create lcs.rs

---
+++ dp_snippets/lcs.rs
@@ -0,0 +1,33 @@
+fn main() {
+    let mut sc = Scanner::new();
+    let mut S:Vec<char> = sc.next::<String>().chars().collect();
+    let mut T:Vec<char> = sc.next::<String>().chars().collect();
+    
+    let mut dp = vec![vec![0usize;T.len()+1];S.len()+1];
+    for i in 1..=S.len(){
+        for j in 1..=T.len(){
+            if S[i-1]==T[j-1]{
+                dp[i][j] = dp[i-1][j-1]+1;
+            } else{
+                dp[i][j] = dp[i-1][j].max(dp[i][j-1]);
+            }
+        }
+    }
+    
+    //復元
+    let (mut i, mut j) = (S.len(),T.len());
+    let mut ans = vec![];
+    while (i > 0) && (j > 0){
+        if S[i-1]==T[j-1]{
+            ans.push(S[i-1]);
+            i -= 1; j -= 1;
+        } else if dp[i-1][j] > dp[i][j-1]{
+            i -= 1;
+        } else{
+            j -= 1;
+        }
+    }
+    ans.reverse();
+    println!("{}", ans.iter().join(""));
+    //eprintln!("{:?}",dp);
+}
