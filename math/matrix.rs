2c867d40fbf80991404f496b59112f075077fd79
GaLLium-git <205247616+GaLLium-git@users.noreply.github.com>
2026-01-24 20:56:41 +0900

    Create matrix.rs

---
+++ math/matrix.rs
@@ -0,0 +1,16 @@
+pub struct Matrix{
+    mat: Vec<Vec<Mint>>,
+}
+
+impl Matrix{
+    fn new() -> Self{
+        
+    }
+    fn mul(&self, &rhs:Matrix) -> Self{
+        
+    }
+    
+    fn pow(&self, n:usize) -> Self{
+        
+    }
+}
