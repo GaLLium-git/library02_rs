62557eead10c86004bed06ccea7c12edb0db0b3d
GaLLium-git <205247616+GaLLium-git@users.noreply.github.com>
2026-01-31 18:05:35 +0900

    Update lcs.rs

--- dp_snippets/lcs.rs
+++ dp_snippets/lcs.rs
@@ -1,12 +1,12 @@
 fn main() {
     let mut sc = Scanner::new();
-    let mut S:Vec<char> = sc.next::<String>().chars().collect();
-    let mut T:Vec<char> = sc.next::<String>().chars().collect();
+    let mut S:Vec<char> = sc.next::<String>().chars().collect();S.insert(0,'');
+    let mut T:Vec<char> = sc.next::<String>().chars().collect();S.insert(0,'');
     
-    let mut dp = vec![vec![0usize;T.len()+1];S.len()+1];
-    for i in 1..=S.len(){
-        for j in 1..=T.len(){
-            if S[i-1]==T[j-1]{
+    let mut dp = vec![vec![0usize;T.len()];S.len()];
+    for i in 1..S.len(){
+        for j in 1..T.len(){
+            if S[i]==T[j]{
                 dp[i][j] = dp[i-1][j-1]+1;
             } else{
                 dp[i][j] = dp[i-1][j].max(dp[i][j-1]);
@@ -18,8 +18,8 @@ fn main() {
     let (mut i, mut j) = (S.len(),T.len());
     let mut ans = vec![];
     while (i > 0) && (j > 0){
-        if S[i-1]==T[j-1]{
-            ans.push(S[i-1]);
+        if S[i]==T[j]{
+            ans.push(S[i]);
             i -= 1; j -= 1;
         } else if dp[i-1][j] > dp[i][j-1]{
             i -= 1;
