4a84170e6252a6738849da3a8846b2f63d01a0f5
GaLLium-git <205247616+GaLLium-git@users.noreply.github.com>
2026-02-01 10:50:03 +0900

    Update lis.rs

--- dp_snippets/lis.rs
+++ dp_snippets/lis.rs
@@ -1,13 +1,13 @@
 pub fn lis(A:&Vec<usize>) -> usize{
    let N = A.len();
-   let mut lis = vec![usize::MAX;N];
+   let mut dp = vec![usize::MAX;N];
    for &x in A.iter(){
-     let pos = bsearch
-     lis[pos] = i;
+     let pos = bsearch_usize(0..N,|i| dp[i] > x);
+     dp[pos] = x;
    }
    let mut res = 0;
    for i in 0..N{
-       if lis[i] == usize::MAX {break;}
+       if dp[i] == usize::MAX {break;}
        res = i+1;
    }
    res
